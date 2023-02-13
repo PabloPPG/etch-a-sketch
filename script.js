6da5d46b63cc6aa0136b27b543f67f3286c6f1e8
PabloPPG <pabloppg1992@gmail.com7.noreply.github.com>
2023-02-13 21:10:12 +0100

    Add black drag color. Still need work

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const sketcher = document.getElementById("sketcher");
-const docFrag = document.createDocumentFragment();
-// const newRow = document.createElement("div");
-// const newTile = document.createElement("div");
-// newRow.className = "row";
-// newTile.className = "tile";
+const lowButton = document.getElementById("low");
+const normalButton = document.getElementById("normal");
+const highButton = document.getElementById("high");
+const higherButton = document.getElementById("higher");
+const tile = document.getElementsByClassName("tile");
+console.log(tile);
+
+function deleteGrid() {
+  sketcher.replaceChildren();
+}
+
+function defaultGrid() {
+  const num = 16;
+  for (let i = 0; i < num; i++) {
+    const newRow = document.createElement("div");
+    newRow.className = "row";
+    for (let j = 0; j < num; j++) {
+      const newTile = document.createElement("div");
+      newTile.className = "tile";
+      newRow.appendChild(newTile);
+    }
+    sketcher.appendChild(newRow);
+  }
+}
 
 function createGrid(num) {
   for (let i = 0; i < num; i++) {
@@ -14,9 +33,34 @@ function createGrid(num) {
       newTile.className = "tile";
       newRow.appendChild(newTile);
     }
-    docFrag.appendChild(newRow);
+    sketcher.appendChild(newRow);
   }
-  sketcher.appendChild(docFrag);
 }
 
-createGrid(16);
+defaultGrid();
+
+//LISTENERS
+//button 12x12 grid
+lowButton.addEventListener("click", () => {
+  deleteGrid();
+  createGrid(10);
+});
+
+normalButton.addEventListener("click", () => {
+  deleteGrid();
+  createGrid(16);
+});
+
+highButton.addEventListener("click", () => {
+  deleteGrid();
+  createGrid(24);
+});
+
+higherButton.addEventListener("click", () => {
+  deleteGrid();
+  createGrid(40);
+});
+
+for (let i = 0; i < [...tile].length; i++) {
+  tile[i].addEventListener("mousemove", () => tile[i].classList.add("toBlack"));
+}
